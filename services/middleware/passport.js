620a1ed26ba0a437f65f2dac2815a16571f4011e
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-05-14 14:52:23 +0200

    Added avatar from google login

--- services/middleware/passport.js
+++ services/middleware/passport.js
@@ -16,62 +16,49 @@ function createToken(author, id) {
     return accessToken;
   }  
 
-// Opzioni per configurare il servizio OAuth di Google
 const options = {
-  // Client ID perso dalla console di Google alla registrazione dell'applicazione
   clientID: process.env.CLIENT_ID,
-  // Client Secret preso dalla console di Google alla registrazione dell'applicazione
   clientSecret: process.env.CLIENT_SECRET,
-  // Callback da eseguire quando un'utente effettua l'autenticazione all'endpoint
   callbackURL: process.env.REACT_APP_BACKEND_URL + process.env.CB,
 };
-// Crea un'istanza di Google Strategy
+
 const googleStrategy = new GoogleStrategy(
   options,
   async (_accessToken, _refreshToken, profile, passportNext) => {
     try {
-      // Destrutturiamo l'oggetto profile e prendiamo i dati che ci servono
       const { email, given_name, family_name, sub, picture } = profile._json;
-        
-      // Verifica se l'utente esiste già nelò database
+      
+      console.log("picture è ????")
+      console.log(picture)
+      console.log("-------------------")
       const author = await Author.findOne({ email });
 
-      // L'utente esiste già nel DB?
       if (author) {
-        // L'utente esiste già nel DB
-
-        // Creiamo il token di accesso, utilizzando il servizio di GoogleStrategy
         const accToken = createToken({
           _id: author._id,
         });
 
-        // Chiamiamo la callback, passando null come errore e accToken come secondo parametro
-        passportNext(null, { accToken });
+       passportNext(null, { accToken });
       } else {
-        // L'utente non esiste nel DB
-
-        // Crea un nuovo utente
-        const newAuthor = new Author({
+       const newAuthor = new Author({
           name: given_name,
           lastName: family_name,
           email: email, 
           googleId: sub,
+          avatar: picture
         });
 
         console.log("il nuovo autore che viene creato : ")
         console.log(newAuthor)
         console.log("--------------------------------- ")
 
-        // Salva l'utente nel database
         await newAuthor.save();
 
-        // Genera token
-        const accToken = createToken({
+         const accToken = createToken({
           name: newAuthor.name,
           _id: newAuthor._id 
         });
 
-        // Chiamiamo la callback, passando null come errore e accToken come secondo parametro
         passportNext(null, { accToken });
       }
     } catch (error) {
